61483901d3a0efd6442fe18c758e8954080cdeee
Andrew Cohn <andrewcohn0@gmail.com>
2018-05-29 10:50:32 -0400

    destructure props in poke collection

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,10 @@ import React from 'react'
 import PokemonCard from './PokemonCard'
 import { Card } from 'semantic-ui-react'
 
-const PokemonCollection = props => (
+const PokemonCollection = ({ pokemon, toggleImage }) => (
   <Card.Group itemsPerRow={6}>
-    {props.pokemon.map(pokemon => (
-      <PokemonCard key={pokemon.id} pokemon={pokemon} toggleImage={props.toggleImage} />
+    {pokemon.map(pokemon => (
+      <PokemonCard key={pokemon.id} pokemon={pokemon} toggleImage={toggleImage} />
     ))}
   </Card.Group>
 )
