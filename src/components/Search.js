10b7e392b50e93d13ae6e25638a73ff458c55689
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2020-06-25 17:35:01 -0400

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ const Search = props => {
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt" onChange={() => {console.log("searching...")}} />
+        <input className="prompt"/>
         <i className="search icon" />
       </div>
     </div>
